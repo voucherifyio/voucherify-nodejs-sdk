840f2a547c6b5bf96afb3705c874d69d3af5b898
Tomasz Sikora <frakti7@gmail.com>
2018-12-28 10:58:54 +0100

    Update tests with new val rules structure

--- test/validation-rules-api.spec.js
+++ test/validation-rules-api.spec.js
@@ -15,45 +15,55 @@ describe('Validation Rules API', function () {
   it('should create validation rule', function (done) {
     var server = nock('https://api.voucherify.io', reqWithBody)
             .post('/v1/validation-rules', {
-              voucher_code: 'test-code',
-              redemptions: {
-                junction: 'AND',
-                count_per_customer: {
+              name: 'Redeemable Once for new Customers',
+              rules: {
+                '1': {
+                  name: 'customer.segment',
                   conditions: {
-                    $is: [
-                      1
-                    ]
+                    $is: ['seg_n3vVcU5t0m3rs4rEPr3C1oU5']
                   }
-                }
+                },
+                '2': {
+                  name: 'redemption.count.per_customer',
+                  conditions: {
+                    $less_than_or_equal: [1]
+                  }
+                },
+                logic: '1 and 2'
               }
             })
             .reply(200, {})
 
     client.validationRules.create({
-      voucher_code: 'test-code',
-      redemptions: {
-        junction: 'AND',
-        count_per_customer: {
+      name: 'Redeemable Once for new Customers',
+      rules: {
+        '1': {
+          name: 'customer.segment',
           conditions: {
-            $is: [
-              1
-            ]
+            $is: ['seg_n3vVcU5t0m3rs4rEPr3C1oU5']
           }
-        }
+        },
+        '2': {
+          name: 'redemption.count.per_customer',
+          conditions: {
+            $less_than_or_equal: [1]
+          }
+        },
+        logic: '1 and 2'
       }
     })
-            .then(function () {
-              server.done()
-              done()
-            })
+    .then(function () {
+      server.done()
+      done()
+    })
   })
 
   it('should get validation rule', function (done) {
     var server = nock('https://api.voucherify.io', reqWithoutBody)
-            .get('/v1/validation-rules/cust_test-id')
+            .get('/v1/validation-rules/val_QvHPi3B7hyey')
             .reply(200, {})
 
-    client.validationRules.get('cust_test-id')
+    client.validationRules.get('val_QvHPi3B7hyey')
             .then(function () {
               server.done()
               done()
@@ -62,45 +72,57 @@ describe('Validation Rules API', function () {
 
   it('should update validation rule', function (done) {
     var server = nock('https://api.voucherify.io', reqWithBody)
-            .put('/v1/validation-rules/cust_test-id', {
-              redemptions: {
-                junction: 'AND',
-                count_per_customer: {
+            .put('/v1/validation-rules/val_QvHPi3B7hyey', {
+              name: 'Redeemable twice for new Customers',
+              rules: {
+                '1': {
+                  name: 'customer.segment',
                   conditions: {
-                    $is: [
-                      2
-                    ]
+                    $is: ['seg_n3vVcU5t0m3rs4rEPr3C1oU5']
                   }
-                }
+                },
+                '2': {
+                  name: 'redemption.count.per_customer',
+                  conditions: {
+                    $less_than_or_equal: [2]
+                  }
+                },
+                logic: '1 and 2'
               }
             })
             .reply(200, {})
 
     client.validationRules.update({
-      id: 'cust_test-id',
-      redemptions: {
-        junction: 'AND',
-        count_per_customer: {
+      id: 'val_QvHPi3B7hyey',
+      name: 'Redeemable twice for new Customers',
+      rules: {
+        '1': {
+          name: 'customer.segment',
           conditions: {
-            $is: [
-              2
-            ]
+            $is: ['seg_n3vVcU5t0m3rs4rEPr3C1oU5']
           }
-        }
+        },
+        '2': {
+          name: 'redemption.count.per_customer',
+          conditions: {
+            $less_than_or_equal: [2]
+          }
+        },
+        logic: '1 and 2'
       }
     })
-            .then(function () {
-              server.done()
-              done()
-            })
+    .then(function () {
+      server.done()
+      done()
+    })
   })
 
   it('should delete validation rule', function (done) {
     var server = nock('https://api.voucherify.io', reqWithoutBody)
-            .delete('/v1/validation-rules/cust_test-id')
+            .delete('/v1/validation-rules/val_Z755MnH1Xow1')
             .reply(200, {})
 
-    client.validationRules.delete('cust_test-id')
+    client.validationRules.delete('val_Z755MnH1Xow1')
             .then(function () {
               server.done()
               done()

102f51bb86f6672c4ab7bbef7bcbe32902ea6431
Tomasz Sikora <frakti7@gmail.com>
2019-11-22 11:29:26 +0100

    Update lodash

--- src/Customers.js
+++ src/Customers.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const {encode, isFunction} = require('./helpers')
-const omit = require('lodash.omit')
+const omit = require('lodash/omit')
 
 module.exports = class Customers {
   constructor (client) {

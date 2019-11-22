c64af47cfd7fba5e168e8993b9715b5025e2f1c7
Tomasz Sikora <frakti7@gmail.com>
2019-11-22 13:44:38 +0100

    Drop deprecated methods

--- src/Events.js
+++ src/Events.js
@@ -1,24 +1,10 @@
 'use strict'
 
-const { isFunction, isObject } = require('./helpers')
+const { isObject } = require('./helpers')
 
 module.exports = class Events {
   constructor (client) {
     this.client = client
-  }
-
-  track (eventName, metadata, customer, callback) {
-    console.warn('This track method invocation is deprecated. Please use `events.create()` instead. Method will be removed in ver. 3.0')
-    if (isFunction(customer)) {
-      callback = customer
-      customer = {}
-    }
-
-    return this.client.post('/events', {
-      event: eventName,
-      customer,
-      metadata
-    }, callback)
   }
 
   create (eventName, params, callback) {
--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ const Rewards = require('./Rewards')
 const Loyalties = require('./Loyalties')
 const ValidationRules = require('./ValidationRules')
 const Segments = require('./Segments')
-const { assertOption, isFunction } = require('./helpers')
+const { assertOption } = require('./helpers')
 
 module.exports = function (options) {
   assertOption(options, 'applicationId')
@@ -43,33 +43,12 @@ module.exports = function (options) {
   const segments = new Segments(client)
   const validationRules = new ValidationRules(client)
 
-  /**
-   * Copy redemptions.list method and extend it so we can run:
-   *
-   *   // deprecated
-   *   api.redemptions(query, callback)
-   *
-   *   // new
-   *   api.redemptions.list(query, callback)
-   *   api.redemptions.redeem(code, trackingId, callback)
-   *   // ...
-   */
-  const backwardCompatibleRedemptions = redemptions.list.bind(redemptions)
-  // copy to func object all redemption methods bound to it's context
-  const exposedFunctions = Object.getOwnPropertyNames(Object.getPrototypeOf(redemptions))
-
-  for (const name of exposedFunctions) {
-    if (name !== 'constructor' && isFunction(redemptions[name])) {
-      backwardCompatibleRedemptions[name] = redemptions[name].bind(redemptions)
-    }
-  }
-
   return {
     vouchers,
     campaigns,
     distributions,
     validations,
-    redemptions: backwardCompatibleRedemptions,
+    redemptions,
     promotions,
     customers,
     orders,
@@ -78,45 +57,6 @@ module.exports = function (options) {
     loyalties,
     segments,
     validationRules,
-    events,
-
-    // leaving for backward compatibility
-
-    // vouchers
-    list: (query, callback) => vouchers.list(query, callback),
-    get: (code, callback) => vouchers.get(code, callback),
-    create: (voucher, callback) => vouchers.create(voucher, callback),
-    delete: (code, params, callback) => vouchers.delete(code, params, callback),
-    update: (voucher, callback) => vouchers.update(voucher, callback),
-    enable: (code, callback) => vouchers.enable(code, callback),
-    disable: (code, callback) => vouchers.disable(code, callback),
-    publish: (campaignName, callback) => distributions.publish(campaignName, callback),
-    // validations
-    validate: (code, params, callback) => validations.validateVoucher(code, params, callback),
-    // redemptions
-    redemption: (code, callback) => redemptions.getForVoucher(code, callback),
-    redeem: (code, trackingId, callback) => redemptions.redeem(code, trackingId, callback),
-    rollback: (redemptionId, data, callback) => redemptions.rollback(redemptionId, data, callback),
-    // campaigns
-    campaign: {
-      voucher: {
-        create: (campaignName, voucher, callback) => campaigns.addVoucher(campaignName, voucher, callback)
-      }
-    },
-    // customers
-    customer: customers,
-    // products
-    product: {
-      create: (product, callback) => products.create(product, callback),
-      get: (productId, callback) => products.get(productId, callback),
-      update: (product, callback) => products.update(product, callback),
-      delete: (productId, callback) => products.delete(productId),
-      sku: {
-        create: (productId, sku, callback) => products.createSku(productId, sku, callback),
-        get: (productId, skuId, callback) => products.getSku(productId, skuId, callback),
-        update: (productId, sku, callback) => products.updateSku(productId, sku, callback),
-        delete: (productId, skuId, callback) => products.deleteSku(productId, skuId, callback)
-      }
-    }
+    events
   }
 }
